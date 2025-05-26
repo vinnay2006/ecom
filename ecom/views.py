2ee3a42fab76ff88379a14dae01b6e36c89ee097
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2025-05-26 18:00:30 +0530

    added more features-made folders like templates,staticand made some functions like about ,contact and more

--- ecom/views.py
+++ ecom/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return render( request,"blog/index.htm")
+    return HttpResponse( "welcome to home page !!")
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,19 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("hello!!")
+    return render( request,"shop/index.htm")
+
+def about(request):
+    return HttpResponse("we are at abouts")
+def contact(request):
+    return HttpResponse("we are at abouts")
+def tracker(request):
+    return HttpResponse("we are at abouts")
+def search(request):
+    return HttpResponse("we are at abouts")
+def prodView(request):
+    return HttpResponse("we are at abouts")
+def checkout(request):
+    return HttpResponse("we are at abouts")
+
 
