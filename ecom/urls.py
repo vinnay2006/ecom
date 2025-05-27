3664c3f2f8e34a399d2889d471839f4f39146ac1
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2025-05-28 00:02:04 +0530

    i have done some desiging of my shop home page,and also made a media directory

--- ecom/urls.py
+++ ecom/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.views.generic import RedirectView
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('shop/', include('shop.urls')),
      path('blog/', include('blog.urls')),  
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
