d4967cc83785d367d4326d68a8542cbd488b7773
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2025-05-29 23:46:18 +0530

    made some morechanges in the website front end by putting category wise carousel slides

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from  .models import Product
+from math import ceil
 def index(request):
-    return render( request,"shop/index.htm")
+    products=Product.objects.all()
+    print(products)
+    #params={'no_of_slides':nslides,'range':range(1,len(products)),'product':products}
+
+    allprods=[]
+    catprods=Product.objects.values('category','id')
+    cats={item['category'] for item in catprods}
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n=len(prod)
+        nslides= n//4+ceil((n/4)-(n//4))
+        allprods.append([prod,range(1,nslides),nslides])
+
+    params={'allprods':allprods}
+    return render( request,"shop/index.htm",params)
+    
 
 def about(request):
-    return HttpResponse("we are at abouts")
+    return render( request,"shop/about.htm")
 def contact(request):
     return HttpResponse("we are at abouts")
 def tracker(request):
